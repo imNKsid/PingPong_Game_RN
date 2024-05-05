af4714a6d70c343375a73f6dca0ae24ff2a5e358
Nitesh Kumar <imnksid@gmail.com>
2024-05-06 00:42:49 +0530

    Made the ball bounce when it hits vertical walls

--- With_Expo_CLI/src/screens/Home.tsx
+++ With_Expo_CLI/src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, useWindowDimensions } from "react-native";
 import React, { useEffect } from "react";
 import Animated, {
   Easing,
@@ -12,9 +12,11 @@ const DELTA = 1000 / FPS;
 const SPEED = 1; //0.3;
 
 const Home = () => {
-  const targetPositionX = useSharedValue(0);
-  const targetPositionY = useSharedValue(0);
+  const targetPositionX = useSharedValue(200);
+  const targetPositionY = useSharedValue(200);
   const direction = useSharedValue(normalizeVector({ x: 0, y: -1 }));
+
+  const { height, width } = useWindowDimensions();
 
   useEffect(() => {
     const interval = setInterval(update, DELTA);
@@ -23,6 +25,14 @@ const Home = () => {
   }, []);
 
   const update = () => {
+    const nextX = targetPositionX.value + direction.value.x * SPEED;
+    const nextY = targetPositionY.value + direction.value.y * SPEED;
+
+    if (nextY < 0 || nextY > height) {
+      console.log("Ball hits the vertical wall");
+      direction.value = { x: direction.value.x, y: -direction.value.y };
+    }
+
     targetPositionX.value = withTiming(
       targetPositionX.value + direction.value.x * SPEED,
       {
@@ -47,9 +57,9 @@ const Home = () => {
   });
 
   return (
-    <View>
+    <>
       <Animated.View style={[styles.ball, ballAnimatedStyles]} />
-    </View>
+    </>
   );
 };
 
