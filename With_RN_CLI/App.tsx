f09c0b43ae18bc733dd1d9f0dcb41e4043bb0aaf
Nitesh Kumar <imnksid@gmail.com>
2024-05-23 17:17:03 +0530

    Implemented Level upgradation with player & bgColor change

--- With_RN_CLI/App.tsx
+++ With_RN_CLI/App.tsx
@@ -18,8 +18,5 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
